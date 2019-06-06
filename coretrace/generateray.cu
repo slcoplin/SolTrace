76e117220c7f789bb9dcdb037112d807a5f62fbe
slcoplin <sophiacoplin@gmail.com>
2019-06-06 00:41:26 -0700

    Compiling generateray

--- coretrace/generateray.cu
+++ coretrace/generateray.cu
@@ -51,7 +51,6 @@
 #include <math.h>
 
 #include "types.h"
-#include "mathproc_gpu.cuh"
 
 
 // For cudaMalloc
@@ -69,6 +68,87 @@
 // TODO wrap calls?
 
 #define SEED 179
+
+
+__device__
+void MatrixVectorMultGPU(double M[9], double V[3], double MxV[3])
+{
+    /*{Purpose: To perform multiplication of a matrix (3,3 in row-major) and
+                a vector (3) to result in new vector (3)
+
+              Input -
+                    Matrix = Matrix
+                    Vector = Vector
+              Output -
+                     MxV = Matrix*Vector = new vector}*/
+
+    MxV[0] = M[0] * V[0] + M[1] * V[1] + M[2] * V[2];
+    MxV[1] = M[3] * V[0] + M[4] * V[1] + M[5] * V[2];
+    MxV[2] = M[6] * V[0] + M[7] * V[1] + M[8] * V[2];
+}
+
+
+__device__
+void TransformToLocalGPU(double PosRef[3], double CosRef[3], double Origin[3],
+    double RRefToLoc[9],
+    double PosLoc[3], double CosLoc[3])
+{
+    /*{Purpose:  To perform coordinate transformation from reference system to local
+               system.
+               Input -
+                     PosRef = X,Y,Z coordinates of ray point in reference system
+                     CosRef = Direction cosines of ray in reference system
+                     Origin = X,Y,Z coordinates of origin of local system as measured
+                              in reference system
+                     RRefToLoc = Rotation matrices required for coordinate transform
+                                 from reference to local
+               Output -
+                     PosLoc = X,Y,Z coordinates of ray point in local system
+                     CosLoc = Direction cosines of ray in local system }*/
+
+    double PosDum[3];
+
+    /*{Multiply the position vector and the direction cosine vector by the transformation
+     matrix to get the new vectors in the local system.  The position vector is first
+     referenced to the origin of the local frame.}*/
+    for (int i = 0; i < 3; i++)
+        PosDum[i] = PosRef[i] - Origin[i];
+
+    MatrixVectorMultGPU(RRefToLoc, PosDum, PosLoc);
+    MatrixVectorMultGPU(RRefToLoc, CosRef, CosLoc);
+}
+//end of procedure--------------------------------------------------------------
+
+__device__
+void TransformToReferenceGPU(double PosLoc[3], double CosLoc[3], double Origin[3],
+    double RLocToRef[9],
+    double PosRef[3], double CosRef[3])
+{
+    /*{Purpose:  To perform coordinate transformation from local system to reference
+               system.
+               Input -
+                     PosLoc = X,Y,Z coordinates of ray point in local system
+                     CosLoc = Direction cosines of ray in Loc system
+                     Origin = X,Y,Z coordinates of origin of local system as measured
+                              in reference system
+                     RLocToRef = Rotation matrices required for coordinate transform
+                                 from local to reference (inverse of reference to
+                                 local transformation)
+               Output -
+                     PosRef = X,Y,Z coordinates of ray point in reference system
+                     CosRef = Direction cosines of ray in reference system}*/
+
+    double PosDum[3];
+
+    /*{Use previously calculated RLocToRef matrix (in TransformToLocal) to obtain the
+     inverse transformation back to Reference system.}*/
+    MatrixVectorMultGPU(RLocToRef, PosLoc, PosDum);
+    MatrixVectorMultGPU(RLocToRef, CosLoc, CosRef);
+
+    for (int i = 0; i < 3; i++)
+        PosRef[i] = PosDum[i] + Origin[i];
+}
+
 
 __global__
 void ray_kernel(double MinXSun,
@@ -168,3 +248,5 @@ void generate_rays(TSystem *System,
 
   System->SunRayCount = NumberOfRays;
 }
+
+
